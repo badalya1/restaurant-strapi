b733444479181b3c341608caa393c8bbcdea263f
David Badalyan <badalyandavo@gmail.com>
2023-03-02 17:17:05 +0400

    Initial Commit

---
+++ src/api/category/content-types/category/lifecycle.ts
@@ -0,0 +1,35 @@
+import slugify from "slugify";
+import  { ApplicationError } from "@strapi/utils";
+
+module.exports = {
+  async beforeCreate(event) {
+    await generateSlug(event);
+  },
+
+  async beforeUpdate(event) {
+    await generateSlug(event);
+  },
+};
+
+const generateSlug = async (event) => {
+  const DEFAULT_LOCALE = "en";
+  console.log("Generating slug", event);
+  const { data } = event.params;
+  const id = event.params?.where?.id ?? null;
+  const locale = !id ? "en" : await getLocale(id);
+
+  //Generate slug for en locale only
+  if (!data.slug && data.title && locale == DEFAULT_LOCALE) {
+    data.slug = slugify(data.title, { lower: true });
+  }
+
+  if (!data.slug) {
+    throw new ApplicationError("Slug is required!");
+  }
+};
+
+const getLocale = async (id) => {
+  const res = await strapi.service("api::category.category").findOne(id, {});
+
+  return res.locale;
+};
